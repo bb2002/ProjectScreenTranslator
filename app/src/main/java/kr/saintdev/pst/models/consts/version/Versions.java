494936b96ec86abd64231ff2632223885b0eabf1
5252b <5252bb@daum.net>
2018-07-20 01:32:00 +0900

    07.20 01:30 Release Eli v5.1.1 - 07.18
    최종 배포 완료.

--- app/src/main/java/kr/saintdev/pst/models/consts/version/Versions.java
+++ app/src/main/java/kr/saintdev/pst/models/consts/version/Versions.java
@@ -20,6 +20,7 @@ public class Versions {
         int E_OPEN_BETA_500 = 500;      // Open Beta 5.0.0
         int E_RELEASE_501 = 501;      // Release 5.0.1
         int E_RELEASE_511 = 511;      // Release 5.1.1
+        int E_RELEASE_512 = 512;      // Release 5.1.2
     }
 
     /**
@@ -34,7 +35,7 @@ public class Versions {
      * @return 현재 SDK 버전 코드를 리턴합니다.
      */
     public static int getVersionCode() {
-        return SDK_LEVEL.E_RELEASE_511;
+        return SDK_LEVEL.E_RELEASE_512;
     }
 
     /**
@@ -42,6 +43,6 @@ public class Versions {
      * @return 현재 버전 문자열을 리턴합니다.
      */
     public static String getVersionString() {
-        return "Release v5.1.1 - 07.18";
+        return "Release v5.1.2 - 07.20";
     }
 }
